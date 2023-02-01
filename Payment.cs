ac9d0e343133d5fba04cee26c7c50bbf6532d983
Lis_Alexander <112865418+LisAlexander1@users.noreply.github.com>
2023-02-01 03:13:14 +0200

    Добавьте файлы проекта.

---
+++ Payment.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Payment
+    {
+        private DateOnly _admission;
+        private double _salary;
+        private double _bonus;
+        private int _workedDaysInMonth;
+        private int _workingDaysInMonth;
+        private double _accruedAmount;
+        private double _retainedAmount;
+
+        public Payment(string fullName, DateOnly admission, double salary, double bonus, int tax, int workingDaysInMonth, int workedDaysInMonth)
+        {
+            FullName = fullName;
+            Admission = admission;
+            Salary = salary;
+            Bonus = bonus;
+            Tax = tax;
+            WorkingDaysInMonth = workingDaysInMonth;
+            WorkedDaysInMonth = workedDaysInMonth;
+        }
+
+        public string FullName { get; set; }
+        public int Tax { get; set; }
+
+
+        public DateOnly Admission
+        {
+            get => _admission;
+            set
+            {
+                if (value < DateOnly.FromDateTime(DateTime.Now))
+                {
+                    _admission = value;
+                    AccruedAmount = Accruals();
+                }
+            }
+        }
+
+        public double Salary
+        {
+            get => _salary; set
+            {
+                if (value > 0)
+                {
+                    _salary = value;
+                    AccruedAmount = Accruals();
+                }
+            }
+        }
+
+        public double Bonus
+        {
+            get => _bonus; set
+            {
+                if (value >= 0)
+                {
+                    _bonus = value;
+                    AccruedAmount = Accruals();
+                }
+            }
+        }
+
+        public int WorkedDaysInMonth
+        {
+            get => _workedDaysInMonth; set
+            {
+                if (value > 0)
+                {
+                    _workedDaysInMonth = value;
+                    AccruedAmount = Accruals();
+                }
+            }
+        }
+
+        public int WorkingDaysInMonth
+        {
+            get => _workingDaysInMonth; set
+            {
+                if (value > 0)
+                {
+                    _workingDaysInMonth = value;
+                    AccruedAmount = Accruals();
+                }
+            }
+        }
+
+        public double AccruedAmount 
+        {
+            get => _accruedAmount; private set
+            { 
+                _accruedAmount = value;
+                RetainedAmount = Retention();
+            }
+        }
+
+        public double RetainedAmount { get => _retainedAmount; private set => _retainedAmount = value; }
+
+        public double Accruals()
+        {
+            if (WorkingDaysInMonth > 0)
+            {
+                return ((double)WorkedDaysInMonth / WorkingDaysInMonth) * Salary * (1 + Bonus / 100);
+            }
+            return 0;
+        }
+
+        public double Retention()
+        {
+            return AccruedAmount * 0.01 + AccruedAmount * Tax / 100;
+        }
+
+        public int Experience()
+        {
+            return DateTime.Now.Year - Admission.Year;
+        }
+
+        public double IssuedOnHand()
+        {
+            return AccruedAmount - RetainedAmount;
+        }
+
+        public static double operator +(Payment pay1, Payment pay2)
+        {
+            return pay1.IssuedOnHand() + pay2.IssuedOnHand();
+        }
+
+        public static double operator -(Payment pay1, Payment pay2)
+        {
+            return pay1.IssuedOnHand() - pay2.IssuedOnHand();
+        }
+
+        public static double operator -(Payment pay1)
+        {
+            return -pay1.IssuedOnHand();
+        }
+
+        public static double operator *(Payment pay1, Payment pay2)
+        {
+            return pay1.IssuedOnHand() * pay2.IssuedOnHand();
+        }
+
+        public static double operator /(Payment pay1, Payment pay2)
+        {
+            return pay1.IssuedOnHand() / pay2.IssuedOnHand();
+        }
+
+        public static bool operator >(Payment pay1, Payment pay2)
+        {
+            return pay1.IssuedOnHand() > pay2.IssuedOnHand();
+        }
+
+        public static bool operator <(Payment pay1, Payment pay2)
+        {
+            return pay1.IssuedOnHand() < pay2.IssuedOnHand();
+        }
+
+        public static bool operator ==(Payment pay1, Payment pay2)
+        {
+            return pay1.IssuedOnHand() == pay2.IssuedOnHand();
+        }
+
+        public static bool operator !=(Payment pay1, Payment pay2)
+        {
+            return pay1.IssuedOnHand() != pay2.IssuedOnHand();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using RandomRusName;
+
+namespace lab2
+{
+    public class Program
+    {
+        
+        private static void Main(string[] args)
+        {
+            var rn = new RandomName();
+            var r = new Random();
+            do
+            {
+                int height = (Console.WindowHeight - 1) / 2;
+                var payments = new Payment[height];
+
+                for (int payment_index = 0; payment_index < payments.Length; payment_index++)
+                {
+                    payments[payment_index] = new Payment(
+                        rn.GetName(),
+                        new DateOnly(r.Next(2000, 2023), r.Next(1, 13), r.Next(1, 29)),
+                        r.NextDouble() * 200000 - 10000,
+                        r.NextDouble() * 100,
+                        13,
+                        22,
+                        r.Next(0, 23));
+
+                    var payment = payments[payment_index];
+                    
+                    Console.WriteLine($"Имя - {payment.FullName}, Устроился - {payment.Admission.ToString("dd/MM/yyyy")}, Оклад - {payment.Salary:F0}, "+
+                        $"Надбавка - {payment.Bonus:F0}%, Налог - {payment.Tax:F0}%\nРабочих дней в месяце - {payment.WorkingDaysInMonth}, " +
+                        $"Отработано - {payment.WorkedDaysInMonth}, Начисления - {payment.AccruedAmount:F0}, Удержания - {payment.RetainedAmount:F0}, На руки - {payment.IssuedOnHand():F0}"
+                        );
+                }
+                Console.ReadKey();
+            } while (true);
+        }
+    }
+}
